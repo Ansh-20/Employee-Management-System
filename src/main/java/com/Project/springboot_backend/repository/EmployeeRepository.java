f8a183b7a39b7629526ba454070bb74a5b674e9d
Ansh Soni <103354566+Ansh-20@users.noreply.github.com>
2024-08-26 22:44:03 +0530

    Delete src directory

--- src/main/java/com/Project/springboot_backend/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Project.springboot_backend.repository;
-
-import com.Project.springboot_backend.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    //all crud database methods
-}
